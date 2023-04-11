ebf44a323c34a02dc4c41b095eba0d596ac98424
Kyle Yannelli <kyleyannelli@gmail.com>
2023-04-11 10:51:28 -0400

    GUI - Adding switching to distortion amp

--- java/src/main/java/basik/kyleyannelli/fx/Controllers/BasikGUIController.java
+++ java/src/main/java/basik/kyleyannelli/fx/Controllers/BasikGUIController.java
@@ -7,9 +7,17 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.IOException;
 
@@ -18,6 +26,15 @@ public class BasikGUIController {
     private final double minKnobRotate = -140;
     private double startingMouseY;
     private boolean  isStandby = false;
+    private boolean isDist = false;
+
+    @FXML
+    private ImageView distortionAmp, distPreKnob, distLowKnob,
+            distMidKnob, distHighKnob, distPostKnob,
+            distResKnob, distPresKnob;
+    @FXML
+    private ImageView reverbKnob, speedKnob, intenKnob;
+
     @FXML
     private ToggleButton distChannelButton;
     @FXML
@@ -64,6 +81,46 @@ public class BasikGUIController {
     public void openPedalsButton(ActionEvent event) throws IOException {
         BasikPedalsGUI basikPedalsGUI = new BasikPedalsGUI();
         basikPedalsGUI.start(new Stage());
+    }
+
+    @FXML
+    public void toggleDistortionChannel(ActionEvent event) throws IOException {
+        fullToggleImage(cleanAmpImage, isDist);
+        fullToggleImage(bassKnobImage, isDist);
+        fullToggleImage(trebleKnobImage, isDist);
+        fullToggleImage(volumeKnobImage, isDist);
+        fullToggleImage(intenKnob, isDist);
+        fullToggleImage(speedKnob, isDist);
+        fullToggleImage(reverbKnob, isDist);
+
+        fullToggleImage(distortionAmp, !isDist);
+        fullToggleImage(distPreKnob, !isDist);
+        fullToggleImage(distLowKnob, !isDist);
+        fullToggleImage(distMidKnob, !isDist);
+        fullToggleImage(distHighKnob, !isDist);
+        fullToggleImage(distPostKnob, !isDist);
+        fullToggleImage(distResKnob, !isDist);
+        fullToggleImage(distPresKnob, !isDist);
+
+        isDist = !isDist;
+
+        putAPIToggleAmp();
+    }
+
+    private void putAPIToggleAmp() throws IOException {
+        HttpClient httpClient = HttpClientBuilder.create().setDefaultRequestConfig(RequestConfig.custom().build()).build();
+        HttpPut request = new HttpPut("http://localhost:30108/cli");
+        HttpEntity entity = MultipartEntityBuilder
+                .create()
+                .addTextBody("command", "4", ContentType.APPLICATION_FORM_URLENCODED)
+                .build();
+        request.setEntity(entity);
+        httpClient.execute(request);
+    }
+
+    private void fullToggleImage(ImageView imageView, boolean bo) {
+        imageView.setDisable(!bo);
+        imageView.setVisible(bo);
     }
 
     @FXML
